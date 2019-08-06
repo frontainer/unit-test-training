5d36ff71ee0527db11bb6320dd40a3345bd92247
frontainer <frontainer@gmail.com>
2019-08-06 16:01:51 +0900

    回答例

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import validate from './validate'
+import validate, { existsPostalCode } from './validate'
 const form = document.getElementById('js-form')
 const postal1 = document.getElementById('js-postal-1')
 const postal2 = document.getElementById('js-postal-2')
 
-form.addEventListener('submit', (e) => {
+let checking = false
+form.addEventListener('submit',  (e) => {
   e.preventDefault()
+  if (checking) return
+  checking = true
+
   const val = `${postal1.value}-${postal2.value}`
   const result = validate(val)
-  if (result.error) {
-    alert(result.message)
-    return
+  if (!result) {
+    checking = false
+    return alert('NG')
   }
-  alert(`OK: ${val}`)
+
+  existsPostalCode(`${postal1.value}${postal2.value}`).then(address => {
+    checking = false
+    alert(`OK: ${val}`)
+  }, () => {
+    checking = false
+    alert('NG')
+  })
 })
--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,20 @@
-export default function validate(val) {
-  if (!val) {
-    return false
-  }
-  return true
+const PATTERN = /^[0-9]{3}-[0-9]{4}$/
+export default val => PATTERN.test(val)
+
+export function existsPostalCode(code) {
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.zipaddress.net/?zipcode=${code}`, {
+      mode: 'cors'
+    }).catch(e => {
+      reject()
+    }).then((res) => {
+      return res.json()
+    }).then(res => {
+      if (res && res.data) {
+        resolve(res.data)
+      } else {
+        reject()
+      }
+    })
+  })
 }
